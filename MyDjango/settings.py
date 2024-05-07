8934c5ac40d682906432483dae5b7490bbd774ba
Emad <soomroemadnexus@gmail.com>
2024-05-08 04:30:05 +0500

    update

--- MyDjango/settings.py
+++ MyDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6*bevplw-fn0uij=3ig!5+(3jy-i$gt61c&p0!)tx0@$@el@z6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
